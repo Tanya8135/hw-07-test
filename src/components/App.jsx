92f77573f61277ec6a9ee59970775265123e581a
tanya8135 <108739694+Tanya8135@users.noreply.github.com>
2023-08-12 20:04:16 +0300

    add theme

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,84 @@
+// import { ContactList } from './ContactList/ContactList';
+// import { ContactForm } from './ContactForm/ContactForm';
+// import { Filter } from './Filter/Filter';
+
+// export function App() {
+//   return (
+//     <div>
+//       <main>
+//         <button>Theme</button>
+//         <h1>Phonebook</h1>
+//         <ContactForm />
+//         <h2> Contacts</h2>
+//         <Filter />
+//         <ContactList />
+//       </main>
+//     </div>
+//   );
+// }
+
+/* import { useState } from 'react';
 import { ContactList } from './ContactList/ContactList';
 import { ContactForm } from './ContactForm/ContactForm';
 import { Filter } from './Filter/Filter';
 
+import css from './App.module.css';
+
 export function App() {
+  const [darkTheme, setDarkTheme] = useState(false);
+
+  const toggleTheme = () => {
+    setDarkTheme(prevTheme => !prevTheme);
+  };
+
+  return (
+    <div className={`${darkTheme ? 'darkTheme' : 'lightTheme'}`}>
+      <main>
+        <button className={css.btnTheme} onClick={toggleTheme}>
+          Theme
+        </button>
+        <h1>Phonebook</h1>
+        <ContactForm />
+        <h2> Contacts</h2>
+        <Filter />
+        <ContactList />
+      </main>
+    </div>
+  );
+} */
+
+import React, { useState, useEffect } from 'react';
+import { ContactList } from './ContactList/ContactList';
+import { ContactForm } from './ContactForm/ContactForm';
+import { Filter } from './Filter/Filter';
+
+import css from './App.module.css';
+
+// import './index.css';
+
+export function App() {
+  const [darkTheme, setDarkTheme] = useState(false);
+
+  const toggleTheme = () => {
+    setDarkTheme(prevTheme => !prevTheme);
+  };
+
+  useEffect(() => {
+    if (darkTheme) {
+      document.body.classList.add('darkTheme');
+      document.body.classList.remove('lightTheme');
+    } else {
+      document.body.classList.remove('darkTheme');
+      document.body.classList.add('lightTheme');
+    }
+  }, [darkTheme]);
+
   return (
     <div>
       <main>
+        <button className={css.btnTheme} onClick={toggleTheme}>
+          Theme
+        </button>
         <h1>Phonebook</h1>
         <ContactForm />
         <h2> Contacts</h2>
